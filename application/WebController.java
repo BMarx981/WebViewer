8825437b521c4eb71e61c9709b2c9bc70b8c4232
Brian Marx <brian.marx@expersolve.local>
2018-11-21 09:23:51 -0500

    Add updateFields method and reload button

--- application/WebController.java
+++ application/WebController.java
@@ -3,6 +3,13 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,12 +22,35 @@ public class WebController implements Initializable {
 	@FXML WebView webView = new WebView();
 	@FXML TextField searchField = new TextField();
 	@FXML Button search = new Button("Search");
+	@FXML Button reload = new Button();
 	WebEngine engine;
+	boolean fieldsSet;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		engine = webView.getEngine();
 		engine.setJavaScriptEnabled(true);
+		engine.getLoadWorker().progressProperty().addListener((o, old, progress) -> updateFields());
+		fieldsSet = false;
+	}
+	
+	private void updateFields() {
+		Document doc = engine.getDocument();
+		if (doc != null && !fieldsSet) {
+			try { 
+				Element inputField = (Element) XPathFactory.newInstance().newXPath().evaluate("//*[@tag='input']", doc, XPathConstants.NODE);
+				if (inputField != null) {
+					NodeList list = engine.getDocument().getElementsByTagName("input");
+					for (int i = 0; i < list.getLength(); i++) {
+						System.out.println(list.item(i).toString());
+					}
+					inputField.setAttribute("value", "NEW TEXT");
+					fieldsSet = true;
+				}
+			} catch (Exception e) {
+				System.out.println("FAIL!!!");
+			}
+		}
 	}
 	
 	public void searchFieldEntered() {
@@ -30,6 +60,11 @@ public class WebController implements Initializable {
 	
 	public void searchButtonPressed() {
 		engine.load("http://localhost:8080/");
+		
+	}
+	
+	public void reloadPressed() {
+		engine.reload();
 	}
 
 }
